bd737395834685c0b19e89e0b6bb8cbece30dadb
unknown <geoffroy.goirand@gmail.com>
2025-04-28 14:42:34 +0200

    second

--- TaskFlow.WebApi/Program.cs
+++ TaskFlow.WebApi/Program.cs
@@ -4,13 +4,23 @@ using TaskFlow.Storage.EFcore.Context;
 using TaskFlow.Storage.EFcore.Repositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-
+using TaskFlow.WebApi.Middleware; // ← pour le middleware d'exceptions (à créer)
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// AJOUT : CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var jwtSettings = builder.Configuration.GetSection("Jwt");
 var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
@@ -34,7 +44,6 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TaskFlowDbContext>(options =>
@@ -52,11 +61,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthentication();
-app.UseAuthorization();
+// AJOUT : Middleware de gestion d'exceptions
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+// AJOUT : Utiliser CORS
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
